0bc2a9234171295638558c60d0ecc914fdcf9a7b
Jack <109398578+kidkazeen@users.noreply.github.com>
2024-05-19 13:38:48 -0500

    change a.out to date_convert

--- chapter_3/date_convert.c
+++ chapter_3/date_convert.c
@@ -5,5 +5,5 @@ int main(void)
     int month, day, year;
     printf("Enter a date (mm/dd/yyyy): ");
     scanf("%d/%d/%d", &month, &day, &year);
-    printf("You entered the date %d%d%d\n", year, month, day);
+    printf("You entered the date %d%.2d%.2d\n", year, month, day);
 }
